65ef2e097c69356b5e48197fdc679c54852d5d91
Derrick Hammer <derrick@derrickhammer.com>
2026-09-06 13:39:19 +0000

    fix(extensions): bump tps-counter version to 1.1.1

--- packages/extensions/extensions/tps-counter/index.ts
+++ packages/extensions/extensions/tps-counter/index.ts
@@ -27,7 +27,7 @@ interface Settings {
 export default class TPSCounterExtension implements Extension {
   static metadata = {
     name: 'TPS Counter',
-    version: '1.1.0',
+    version: '1.1.1',
     description: 'Displays tokens per second for agent responses',
     author: 'wladimiiir',
     iconUrl: 'https://raw.githubusercontent.com/hotovo/aider-desk/refs/heads/main/packages/extensions/extensions/tps-counter/icon.png',
